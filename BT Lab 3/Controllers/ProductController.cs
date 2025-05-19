4338ad66529adfc4c5f483b34feec61bb88b7fe4
Yến Nguyễn <yn040620052@gmail.com>
2025-05-19 22:42:14 +0700

    Add project files.

---
+++ BT Lab 3/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using BT_Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT_Lab_3.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Details()
+        {
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "Sample Product",
+                Price = 19.99m
+            };
+            return View(product);
+        }
+    }
+}
---
+++ BT Lab 3/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace BT_Lab_3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
